b35681b6e48bd0f76fecf3ec50ebce3a57c995bf
abinash-shrestha <154217568+abinash-shrestha@users.noreply.github.com>
2025-08-08 23:53:04 +0545

    added redux store for feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import UserCard from './UserCard';
+import { BASE_URL } from '../utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFeed } from '../utils/feedSlice';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const Feed = () => {
-  return <div>FEED</div>;
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+
+    try {
+      const res = await axios.get(BASE_URL + '/user/feed', {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data.data));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return (
+    <div>
+      <UserCard />
+    </div>
+  );
 };
 
 export default Feed;
